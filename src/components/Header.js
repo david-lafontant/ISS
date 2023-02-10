5c003a8cf43ee3111eea834a7fae97914d9b0026
david-lafontant <lafontant.david@gmail.com>
2023-02-10 15:43:49 -0600

    Setup Header component

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageHeading}>
+        <Image source={require("../../assets/logo3.png")} />
+      </View>
+      <View style={styles.textHeading}>
+        <Text style={styles.heading}>
+          {" "}
+          Direction de l'Immigration et de l'Émigration
+        </Text>
+        <Text style={styles.heading}> Republique d'Haiti</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    backgroundColor: "skyblue",
+    alignItems: "center",
+  },
+  imageHeading: {
+    width: "30%",
+  },
+  textHeading: {
+    width: "60%",
+  },
+  heading: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
