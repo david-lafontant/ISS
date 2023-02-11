810e0d758210f6c07e3c221604f4bad3a328ab36
david-lafontant <lafontant.david@gmail.com>
2023-02-11 14:49:00 -0600

    Display Forms screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, SafeAreaView } from "react-native";
-import Header from "./src/components/Header";
+import Forms from "./src/pages/forms/Forms";
+import Header from "./src/components/header/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Forms />
 
       <StatusBar style="auto" />
     </View>
