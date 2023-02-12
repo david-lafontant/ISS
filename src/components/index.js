6d989680f5cec0d54a244208ac9f62f2cd0812d0
david-lafontant <lafontant.david@gmail.com>
2023-02-12 13:03:27 -0600

    Move index.js to component folder

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as DocumentsInfos } from "./documentsInfos/DocumentsInfos";
+export { default as SubmitInfos } from "./submitInfos/SubmitInfos";
+export { default as ReviewInfos } from "./reviewInfos/ReviewInfos";
+export { default as PersonalInfos } from "./personalInfos/PersonalInfos";
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as DocumentsInfos } from "./components/documentsInfos/DocumentsInfos";
-export { default as SubmitInfos } from "./components/submitInfos/SubmitInfos";
-export { default as ReviewInfos } from "./components/reviewInfos/ReviewInfos";
-export { default as PersonalInfos } from "./components/personalInfos/PersonalInfos";
