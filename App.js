db96ababff4ce1febf9d2a26780c2c50a9cd7068
david-lafontant <lafontant.david@gmail.com>
2023-02-12 16:51:28 -0600

    Setup animated splash screen in App.js

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import React, { useState } from "react";
 import "react-native-gesture-handler";
+import AnimatedSplash from "react-native-animated-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, SafeAreaView } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
@@ -7,19 +9,34 @@ import Header from "./src/components/header/Header";
 import { Forms, Home, Finish } from "./src/pages/index";
 const Drawer = createDrawerNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={Home} />
-          <Drawer.Screen name="Form" component={Forms} />
-          <Drawer.Screen name="Finish" component={Finish} />
-        </Drawer.Navigator>
-      </NavigationContainer>
+  const [loading, setLoading] = useState(false);
 
-      <StatusBar style="auto" />
-    </View>
+  setTimeout(() => {
+    setLoading(true);
+  }, 1000);
+
+  return (
+    <AnimatedSplash
+      translucent={true}
+      isLoaded={loading}
+      logoImage={require("./assets/logo3.png")}
+      backgroundColor={"#262626"}
+      logoHeight={200}
+      logoWidth={200}
+    >
+      <View style={styles.container}>
+        <Header />
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Form" component={Forms} />
+            <Drawer.Screen name="Finish" component={Finish} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+
+        <StatusBar style="auto" />
+      </View>
+    </AnimatedSplash>
   );
 }
 
