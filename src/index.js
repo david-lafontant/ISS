8ddb3cf379f7bc542ce504ff453ce85b10d44074
david-lafontant <lafontant.david@gmail.com>
2023-02-11 18:53:51 -0600

    Implement Barrel Pattern  with index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as DocumentsInfos } from "./components/documentsInfos/DocumentsInfos";
+export { default as SubmitInfos } from "./components/submitInfos/SubmitInfos";
+export { default as ReviewInfos } from "./components/reviewInfos/ReviewInfos";
+export { default as PersonalInfos } from "./components/personalInfos/PersonalInfos";
