fd07d03c36902fa31b69619e6ecd52c7cf9d8450
david-lafontant <lafontant.david@gmail.com>
2023-02-13 09:46:43 -0600

    Add dummy text to home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,30 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
 
 const Home = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Text>Home Screen</Text>
+  <View
+    style={[
+      { flex: 1, alignItems: "center", justifyContent: "center" },
+      styles.container,
+    ]}
+  >
+    <Text>Immigration Scheduling System</Text>
+    <Text style={{ textAlign: "justify" }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum
+    </Text>
     <Button title="Go to Form" onPress={() => navigation.navigate("Form")} />
   </View>
 );
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
