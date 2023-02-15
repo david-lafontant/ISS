e9f0ce5337d5d17c904bd8e2d08bcc8088af591e
david-lafontant <lafontant.david@gmail.com>
2023-02-14 19:29:53 -0600

    Create pickImage function for picking image from phone library

--- src/components/documentsInfos/DocumentsInfos.jsx
+++ src/components/documentsInfos/DocumentsInfos.jsx
@@ -1,7 +1,22 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
 import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const DocumentsInfos = ({ person, setPerson }) => {
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPerson({ ...person, ["photo"]: result.assets[0].uri });
+    }
+  };
+
   return (
     <View style={styles.infoWrapper}>
       <Text>DocumentsInfos</Text>
